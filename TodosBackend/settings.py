77155efa4bb8892e8d07b0d6c6d396d2ad800a2b
Wahyu Setianto <wahyusetianto19@gmail.com>
2021-06-29 13:31:04 +0700

    Change settings.py for ALLOWED_HOST

--- TodosBackend/settings.py
+++ TodosBackend/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'SECRET-KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '0.0.0.0',
+    '127.0.0.1',
+    'localhost',
+    '.herokuapp.com'
+]
 
 
 # Application definition
